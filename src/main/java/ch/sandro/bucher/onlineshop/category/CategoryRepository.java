56e3f98efae691986f01827a993c5bff17f636ff
Sandro Bucher <sandro.bucher@bbzbl-it.ch>
2026-05-04 11:33:20 +0200

    Category Controller angelegt, damit man nach einer Kategorie filtern kann

---
+++ src/main/java/ch/sandro/bucher/onlineshop/category/CategoryRepository.java
@@ -0,0 +1,8 @@
+package ch.sandro.bucher.onlineshop.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
